248c83bdfaa7e56c8814a44f59869cb579b53a6c
Marito <mario@Mario-LT.lan>
2025-02-28 00:31:00 +0000

    Feed en el front

--- feed/serializers.py
+++ feed/serializers.py
@@ -1,8 +1,21 @@
+# serializers.py
 from rest_framework import serializers
 from .models import Post
+from accounts.models import CustomUser
+
 
 class PostSerializer(serializers.ModelSerializer):
+    # Con esta línea tomas el valor 'username' del campo user
+    user_username = serializers.CharField(source="user.username", read_only=True)
+
     class Meta:
         model = Post
-        fields = '__all__'
-        # Si no quieres exponer todos los campos, puedes definirlos manualmente.
+        fields = [
+            "id",
+            "user",  # esto seguirá devolviendo el id del usuario
+            "user_username",  # este nuevo campo mostrará el nombre del usuario
+            "content",
+            "created_at",
+            "image",
+            "video",
+        ]
